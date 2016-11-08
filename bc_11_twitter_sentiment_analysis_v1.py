395661211b487c9a20d9f1f44644824ba85f7e93
ianoti <ianodhiambootieno@gmail.com>
2016-11-08 17:44:16 +0300

    added functionality to store tweets in database, has bug of not rejecting duplicate entries

--- bc_11_twitter_sentiment_analysis_v1.py
+++ bc_11_twitter_sentiment_analysis_v1.py
@@ -2,6 +2,15 @@ import requests
 import bc_11_access_credentials
 import json
 from requests_oauthlib import OAuth1
+import sqlite3
+
+
+conn = sqlite3.connect("twitter_tweets.db")
+conn.execute('''CREATE TABLE IF NOT EXISTS Twitter
+		(TWEET_KEY text NOT NULL,
+			TWEET_SCREEN_NAME text NOT NULL,
+			TWEET_CONTENT text NOT NULL);''')
+conn.close()
 
 url_authenticate = "https://api.twitter.com/1.1/account/verify_credentials.json"
 auth = OAuth1(bc_11_access_credentials.credentials["consumer_key"], bc_11_access_credentials.credentials["consumer_secret"],
@@ -17,9 +26,35 @@ tweet_json = tweet.json()
 # print(json.dumps(tweet_json, sort_keys=True,
 		# indent = 4, separators=(",",":"))) #this prints the formatted JSON response from Twitter API
 total_tweet = []
+total_user = []
+total_id = []
+conn = sqlite3.connect("twitter_tweets.db")
 for i in range(0,len(tweet_json)):
+	tweet_user = tweet_json[i]["user"]["screen_name"]
+	total_user.append(tweet_user)
+
+	tweet_id = tweet_json[i]["id_str"]
+	total_id.append(tweet_id)
+
 	tweet_text = tweet_json[i]["text"]
+	tweet_textenc = tweet_text.encode("utf-8", "ignore") #this encodes the text as utf-8 as is received from Twitter
 	total_tweet.append(tweet_text)
+	conn.execute("INSERT INTO Twitter VALUES(?, ?, ?);", (tweet_id, tweet_user, tweet_textenc))
+	conn.commit()
+
+cursor = conn.execute("SELECT TWEET_KEY, TWEET_SCREEN_NAME, TWEET_CONTENT from Twitter")
+for row in cursor:
+	print("tweet_ID = ", row[0])
+	print("tweet_UserName = ", row[1])
+	print("tweet_Text = ", row[2], "\n")
+conn.close
+
+
 
 all_tweet_text = " ".join(total_tweet)
 print (all_tweet_text)
+print (total_id)
+print (total_user)
+#-----------------------------
+#this section deals with the database
+#-------------------------------

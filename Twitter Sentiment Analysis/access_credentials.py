1f9c59fa270a0743f2b00c3e27843c7ed3a957ba
ianoti <ianodhiambootieno@gmail.com>
2016-11-10 23:53:11 +0300

    A fresh branch of the repo holding final stages of the project

---
+++ Twitter Sentiment Analysis/access_credentials.py
@@ -0,0 +1,10 @@
+consumer_key = "LMHP2mSVzCYIijk1O0I0vmkD1"
+consumer_secret = "Mn3YFC9Wgf5V4jxPoxp54nmKdE5coXelVDNZ0vuybe12FtlV9f"
+access_token = "582659507-aJj2MnmO3mRaCXY5uvf7YS1uIRK7pUNskT3PO8Ut"
+access_token_secret = "RehVuyahciuo03z9nC9iOmoyYxWteV31772b74WqrZX9M"
+
+credentials = {"consumer_key":consumer_key,"consumer_secret":consumer_secret,
+						"access_token":access_token, "access_token_secret":access_token_secret}
+
+Alchemycredentials = {"url": "https://gateway-a.watsonplatform.net/calls",
+  "note": "It may take up to 5 minutes for this key to become active", "apikey": "e0eeff726756669b4c85206655a0b73e050028a9"}

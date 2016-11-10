70a3dd728137374530314044c9e7068d04b6dd02
ianoti <ianodhiambootieno@gmail.com>
2016-11-11 00:05:25 +0300

    deletion of extra files

--- bc_11_access_credentials.py
+++
@@ -1,7 +0,0 @@
-consumer_key = "LMHP2mSVzCYIijk1O0I0vmkD1"
-consumer_secret = "Mn3YFC9Wgf5V4jxPoxp54nmKdE5coXelVDNZ0vuybe12FtlV9f"
-access_token = "582659507-aJj2MnmO3mRaCXY5uvf7YS1uIRK7pUNskT3PO8Ut"
-access_token_secret = "RehVuyahciuo03z9nC9iOmoyYxWteV31772b74WqrZX9M"
-
-credentials = {"consumer_key":consumer_key,"consumer_secret":consumer_secret,
-						"access_token":access_token, "access_token_secret":access_token_secret}
--- stopwords.py
+++
@@ -1,4 +0,0 @@
-from stop_words import get_stop_words
-stopwords = get_stop_words("english")
-# print (stopword_generator)
-stopwords += [".", ",", "?", "*", "/"]
